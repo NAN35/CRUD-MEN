b7e9bb43957bdd0d80bc1cbbecb7cd922c2c02f5
Akshay Kumar <akshaykumarp.b@gmail.com>
2023-02-04 20:35:38 +0530

    adding Router

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require('dotenv').config()
+const alienRouter = require('./routers/aliens')
+require("dotenv").config();
 const MONGODB_URI = process.env.MONGO_LINK;
 
 const app = express();
 async function main() {
-  mongoose.set('strictQuery', false);
+  mongoose.set("strictQuery", false);
   await mongoose.connect(MONGODB_URI);
   console.log("Database connected...");
 }
 
 main().catch((err) => console.log(err));
+
+app.use('/aliens',alienRouter);
+
+app.listen(3000, () => {
+  console.log("Server started in port 3000");
+});
